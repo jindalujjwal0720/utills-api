311663f016b71e7571c8d0493a12c6e181493d56
Ujjwal Jindal <jindalujjwal0720@gmail.com>
2023-07-02 00:35:07 +0530

    manifest - resume

--- src/components/ResumeBuilder/Resume.jsx
+++ src/components/ResumeBuilder/Resume.jsx
@@ -3,10 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import ResumeShowcase from "./showcase/ResumeShowcase";
 import ResumeBuilder from "./builder/ResumeBuilder";
 import ResumeProvider from "./builder/components/context/Resume";
+import { Helmet } from "react-helmet";
 
 const Resume = () => {
   return (
     <ResumeProvider>
+      <Helmet>
+        <title>Resume Builder</title>
+        <meta
+          name="description"
+          content="Resume Builder - Made for ISM with 🧡 by @leafpetal"
+        />
+      </Helmet>
       <Routes>
         <Route path="/showcase" element={<ResumeShowcase />} />
         <Route path="/*" element={<ResumeBuilder />} />
